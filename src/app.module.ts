dd953a63c12dc91ebfd1da544cd7a44eab5f115e
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-01 10:07:00 +0600

    update prisma service and add to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   providers: [],
 })
 export class AppModule {}
