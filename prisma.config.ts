62fdacb51276c30ca6cbaa184695aeca4c5c29e6
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-01 09:46:30 +0600

    setup auth models and migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
+  schema: path.join('prisma'),
   migrations: {
     path: 'prisma/migrations',
   },
